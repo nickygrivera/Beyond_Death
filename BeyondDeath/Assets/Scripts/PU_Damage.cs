16a42b0031f9c9e34b2f124bc1235cca913db76a
trudytru30 <157128336+trudytru30@users.noreply.github.com>
2025-10-15 16:53:07 +0200

    PowerUps bases

---
+++ BeyondDeath/Assets/Scripts/PU_Damage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEngine;
+
+public class PU_Damage : MonoBehaviour
+{
+    private float damageIncial = getDamage(); //coger el getDamage del player
+    private float damageImproved = damageIncial * 1,5f;
+    
+    IEnumerator //corrutina para que lo mantenga durante 6 segundos
+}
---
+++ BeyondDeath/Assets/Scripts/PU_Projectile.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PU_Projectile : MonoBehaviour
+{
+    /*Solo hacer onda expansiva, y se le mete la componente proyectil para que lo instacie con esta habilidad*/
+}
