0c6fa0ccf76c0e9c24c6322f93d84ee0ba9dcd0c
AlvaroGutierrez8755 <alvaro.olmedo@live.u-tad.com>
2025-10-19 04:03:05 +0200

    Avance3

---
+++ BeyondDeath/Assets/Scripts/ScriptTranscionMuerte.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ScriptTranscionMuerte : MonoBehaviour
+{
+
+    private AudioSource cosa;
+    [SerializeField] GameObject messageDead;
+    public float timer = 0;
+    [SerializeField] AudioSource activateAudioGhost;
+   
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cosa = GetComponent<AudioSource>();
+        StartCoroutine(ejecutarSonido());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        cosa.volume -= Time.deltaTime * 0.038f;
+       
+    }
+
+
+
+    IEnumerator ejecutarSonido()
+    {
+        yield return new WaitForSeconds(timer);
+        activateAudioGhost.Play();
+        messageDead.SetActive(true);
+      
+    }
+}
